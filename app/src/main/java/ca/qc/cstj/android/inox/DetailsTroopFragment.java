75db64b34e659b6a1b84751c2e2a4ed0ae950ed6
bigpari <mathieu_pariseau@hotmail.com>
2014-12-20 10:06:48 -0500

    maintenant je vais faire les exploration et troop individuel

--- app/src/main/java/ca/qc/cstj/android/inox/DetailsTroopFragment.java
+++ app/src/main/java/ca/qc/cstj/android/inox/DetailsTroopFragment.java
@@ -22,10 +22,10 @@ import android.view.ViewGroup;
 public class DetailsTroopFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM1 = "Troop_href";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
+    private String mhref;
 
     private OnFragmentInteractionListener mListener;
 
@@ -33,14 +33,14 @@ public class DetailsTroopFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
+     * @param href Parameter 1.
      * @return A new instance of fragment DetailsTroopFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static DetailsTroopFragment newInstance(String param1) {
+    public static DetailsTroopFragment newInstance(String href) {
         DetailsTroopFragment fragment = new DetailsTroopFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM1, href);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,7 +52,7 @@ public class DetailsTroopFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
+            mhref = getArguments().getString(ARG_PARAM1);
         }
     }
 
