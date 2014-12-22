b4eb63656dbf560e738fe9c5edecb9edff5afc9e
bigpari <mathieu_pariseau@hotmail.com>
2014-12-22 12:45:57 -0500

    sfsfsddfss

--- app/src/main/java/ca/qc/cstj/android/inox/ScannerFragment.java
+++ app/src/main/java/ca/qc/cstj/android/inox/ScannerFragment.java
@@ -1,14 +1,11 @@
 package ca.qc.cstj.android.inox;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
